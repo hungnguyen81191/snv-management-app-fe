c7914659c1830c14a0b991cd24ddc89faa3b74e5
hung <hungnguyen81191@gmail.com>
2022-04-13 02:33:39 +0700

    commit 4

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,44 +1,32 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <div class="bg-indigo-600">
-    <div class="px-3 py-3 mx-auto max-w-7xl sm:px-6 lg:px-8">
-      <div class="flex flex-wrap items-center justify-between">
-        <div class="flex items-center flex-1 w-0">
-         
-          <div class="ml-3 font-medium text-white truncate">
-            <router-link to="/user">User</router-link>
-            <router-view/>
-            <button @click="logout">Logout</button>
-          </div>
+  
+    <div class="flex-1 ml-80 px-4 flex justify-between bg-pink-100 ">
+     <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+          <a class="whitespace-nowrap px-4 py-2 text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-300" @click="login"> Login </a>
+          <a class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-300" @click="logout"> Logout </a>
         </div>
-        <div class="flex-shrink-0 order-3 w-full mt-2 mr-1 sm:order-2 sm:mt-0 sm:w-auto">
-          <button hidden:none type="button" class="flex items-center justify-center px-4 py-2 text-sm font-medium text-indigo-600 bg-white border border-transparent rounded-md shadow-sm hover:bg-indigo-50"
-          @click="login">
-           Login </button>
-        </div>
-        
-      </div>
-    </div>
   </div>
+  
 </template>
 
 <script>
- import { useAuth0 } from '@auth0/auth0-vue';
+import { useAuth0 } from "@auth0/auth0-vue";
 
-  export default {
-    setup() {
-      const { loginWithRedirect, user, isAuthenticated, logout } = useAuth0();
+export default {
+  setup() {
+    const { loginWithRedirect, user, isAuthenticated, logout } = useAuth0();
 
-      return {
-        login: () => {
-          loginWithRedirect();
-        },
-        logout: () => {
-          logout({ returnTo: window.location.origin });
-        },
-        user,
-        isAuthenticated
-      };
-    }
-  };
+    return {
+      login: () => {
+        loginWithRedirect();
+      },
+      logout: () => {
+        logout({ returnTo: window.location.origin });
+      },
+      user,
+      isAuthenticated,
+    };
+  },
+};
 </script>
---
+++ src/views/Calendar.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Calendar page.</h1>
+    </div>
+</template>
